e911ae1cf6641d4b163284d9ae83a64e21e853d1
Ankush Kumar Jha <jha.akj05@gmail.com>
2025-09-16 20:28:32 +0530

    Constructor Overloading.cpp

--- Constructor Overloading.cpp
+++ Constructor Overloading.cpp
@@ -1,5 +1,5 @@
-// Aditya Jadhav
-// 24070123005
+// Ankush Jha
+// 24070123016
 
 #include <iostream>
 using namespace std;
@@ -44,4 +44,5 @@ int main() {
 // OUTPUT
 // Shape: Undefined, Area: 0
 // Shape: Rectangle, Area: 50
+
 // Shape: Square, Area: 49
